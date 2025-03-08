ae1a1833132f119f155f78c06bad34909a464568
shayle <shayle@checkpoint.com>
2025-03-08 20:36:25 +0200

    home work

---
+++ python_homework_page42/pythonProject/page42.py
@@ -0,0 +1,6 @@
+wifi = "wifi/eth/network" # I don't have wifi on my computer but I'm also not sure if I understood correctly
+laptops_hostname = "shay"
+my_con = wifi + laptops_hostname
+print(my_con)
+my_con = my_con + " 5324"
+print(my_con)
