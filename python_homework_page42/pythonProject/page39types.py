7c02e78729e1bd3eb9fa0cfa3e6c5cb04c123ec1
shayle <shayle@checkpoint.com>
2025-03-09 11:23:18 +0200

    Practice types

---
+++ python_homework_page42/pythonProject/page39types.py
@@ -0,0 +1,7 @@
+spouse = "Tal"
+print(type(spouse))
+
+spouse_age = 25
+spouse_age_string = str(spouse_age)
+credit_card_details = "123 salty fish"
+print(spouse_age_string + " " + credit_card_details)
